8ba6cf9d3e414e042ac11aed665686a126db4e00
Sally E <sme418@nyu.edu>
2021-11-07 16:43:57 -0500

    fortune function complete

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 var heading;
-var buttonExecution;
-var input;
-var output;
-var songs;
+var runButton;
+var inputName;
+var outputText;
 
 document.addEventListener("DOMContentLoaded", function(){
-  heading = document.getElementByID("title");
-  buttonExecution = document.getElementByID("buttonExecution");
-  output = document.getElementByID("fortuneText");
-  input = document.getElementByID("myInput");
 
-  buttonExecution.addEventListener("click", fortune);
+  heading = document.getElementById("title");
+  runButton = document.getElementById("runButton");
+  outputText = document.getElementById("fortuneText");
+  inputName = document.getElementById("myInput");
+
+  runButton.addEventListener("click", fortune);
+
 });
 
 // 1. Write function called "fortune" that will take one argument (visitor's name)
 // and prints a random "fortune teller style" fortune into an element of your page
 
 function fortune(){
-  songs = ["Hi High", "Girl Front", "Stylish", "favOriTe", "Butterfly", "Heat",
+  var songs = ["Hi High", "Girl Front", "Stylish", "favOriTe", "Butterfly", "Heat",
   "Heart Attack", "You and Me Together", "Kiss Later", "WOW", "Why Not?", "Star",
   "new", "Love Cherry Motion", "Satellite", "Eclipse", "Love Letter", "Let Me In",
   "Oh (Yes I Am)", "love4eva", "rendezvous 18.6y", "Sweet Crazy Love", "Colors",
   "LOONATIC", "frozen", "Egoist", "One & Only", "Love & Live", "Universe", "D-1"];
 
-  
+  var randomSong = songs[Math.floor(Math.random()*songs.length)];
+
+  var inputText = inputName.value;
+
+  outputText.innerHTML = inputText + "'s suggested LOONA song is: " + randomSong;
+
+  // console.log(inputText);
 }
+
 // 2. Write a function called "restyle" that applies new CSS styles to the "fortune"
 // text in a random manner each time it fires. Modify at least three CSS properties
 function restyle(){
